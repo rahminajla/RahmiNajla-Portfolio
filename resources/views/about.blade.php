23bec4b70f5ce7870d118eeb4f94bb9c525f7705
rahminajla <rahmi_nj@mhs.unsyiah.ac.id>
2022-05-16 11:06:54 +0700

    add css file and some php file

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.about')
+
+@include('partials.navbar')
+
+
+@section('content')
+
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.contact')
+
+@include('partials.navbar')
+
+
+@section('content')
+
+
+@endsection
